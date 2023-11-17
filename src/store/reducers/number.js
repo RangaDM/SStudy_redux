b36efb08bb1a8864ae8327d51a31544d3c173e89
Ranga Dananjaya <rangamaithreepala@gmail.com>
2023-11-18 03:15:46 +0530

    v2

--- src/store/reducers/number.js
+++ src/store/reducers/number.js
@@ -1,36 +1,39 @@
 const initialState = {
   number: 0,
+  comArray: [],
 };
 
 const numberActiontype = {
   INC: "increment",
   DEC: "decrement",
-}
+};
 
-export const numberIncrement = (data=1) => {
+export const numberIncrement = (data = 1) => {
   return {
     type: numberActiontype.INC,
     data,
   };
 };
 
-export const numberDecrement = (data=1) => {
+export const numberDecrement = (data = 1) => {
   return {
     type: numberActiontype.DEC,
     data,
   };
-}
+};
 
 export const reducer = (state = initialState, action) => {
   if (action.type === numberActiontype.INC) {
     return {
+      ...state,
       number: state.number + action.data,
     };
   }
-    if (action.type === numberActiontype.DEC) {
-        return {
-        number: state.number - action.data,
-        };
-    }
+  if (action.type === numberActiontype.DEC) {
+    return {
+      ...state,
+      number: state.number - action.data,
+    };
+  }
   return state;
 };
